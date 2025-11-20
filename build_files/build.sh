d1b264ff0051d3a64b2c6fcf2c9f5f810676706e
ManInDark <61268856+ManInDark@users.noreply.github.com>
2025-11-20 15:15:36 +0100

    FIX: nvidia driver

--- build_files/build.sh
+++ build_files/build.sh
@@ -22,7 +22,7 @@ rm /etc/yum.repos.d/vscode.repo
 # https://rpmfusion.org/Howto/NVIDIA
 # https://www.tecmint.com/install-nvidia-drivers-in-linux
 
-dnf5 install -y rpmdevtools akmods
+dnf5 install -y rpmdevtools akmods kmodtool
 dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 		https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf5 install -y akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda
