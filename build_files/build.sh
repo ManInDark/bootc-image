e65cba5536ac02bd415a31a2d6658641ba6b33a1
ManInDark <61268856+ManInDark@users.noreply.github.com>
2025-11-21 23:26:36 +0100

    FIX: build

--- build_files/build.sh
+++ build_files/build.sh
@@ -32,9 +32,4 @@ dnf install -y \
     /nvidia-packages/nvidia-kmod-common-* \
     /nvidia-packages/nvidia-modprobe-5*
 
-dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-		https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf5 install -y xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda
-dnf5 remove  -y rpmfusion-free-release rpmfusion-nonfree-release
-
 dnf5 clean all
