b22acbad11ef2c69a776fc1fa284fada3ed6630e
ManInDark <61268856+ManInDark@users.noreply.github.com>
2025-11-22 22:49:06 +0100

    FIX: add gstreamer1-plugin-libav

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,7 +6,7 @@ cp /ctx/os-release /usr/lib/os-release
 
 dnf5 update -y
 dnf5 install -y man-pages man-db glibc-langpack-en glibc-langpack-de plymouth
-dnf5 install -y --setopt=exclude=gnome-tour,malcontent-control gnome-shell gnome-keyring gnome-keyring-pam gnome-bluetooth gnome-terminal nautilus seahorse evolution totem gnome-extensions-app gnome-browser-connector gnome-shell-extension-appindicator
+dnf5 install -y --setopt=exclude=gnome-tour,malcontent-control gnome-shell gnome-keyring gnome-keyring-pam gnome-bluetooth gnome-terminal nautilus seahorse evolution totem gstreamer1-plugin-libav gnome-extensions-app gnome-browser-connector gnome-shell-extension-appindicator
 dnf5 install -y gnome-calculator gnome-disk-utility gnome-backgrounds
 dnf5 install -y curl git btop nvtop tmux flatpak pciutils tailscale make fzf direnv xsel chromium 7zip
 git clone https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator.git /usr/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com
