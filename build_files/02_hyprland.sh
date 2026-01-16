373a6ab0c2efd8705c43d59d798c3d23d60ce484
ManInDark <61268856+ManInDark@users.noreply.github.com>
2026-01-16 20:35:00 +0100

    feat: add SwayNotificationCenter

--- build_files/02_hyprland.sh
+++ build_files/02_hyprland.sh
@@ -3,6 +3,7 @@
 set -ouex pipefail
 
 dnf5 copr enable -y solopasha/hyprland
+dnf5 copr enable -y erikreider/SwayNotificationCenter
 dnf5 install -y \
     hyprland \
     hypridle \
@@ -10,7 +11,7 @@ dnf5 install -y \
     hyprpolkitagent \
     waybar \
     pavucontrol \
-    cliphist
-
+    cliphist \
+    SwayNotificationCenter-git
 
 echo 'if [[ $(ps aux | grep hyprland | wc -l) -eq 1 ]]; then cd $HOME && echo "launch hyprland?" && read && hyprland ; fi' >> /etc/profile.d/launch_hyprland.sh
