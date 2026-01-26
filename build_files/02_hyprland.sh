5c264ffe905210b346c43594eab953b1488e22f0
ManInDark <61268856+ManInDark@users.noreply.github.com>
2026-01-26 22:05:34 +0100

    feat: add foot

--- build_files/02_hyprland.sh
+++ build_files/02_hyprland.sh
@@ -13,6 +13,7 @@ dnf5 install -y \
     waybar \
     pavucontrol \
     cliphist \
-    SwayNotificationCenter-git
+    SwayNotificationCenter-git \
+    foot
 
 echo 'if [[ $(ps aux | grep hyprland | wc -l) -eq 1 ]]; then cd $HOME && echo "launch hyprland?" && read && systemd-run --user --service-type=exec --unit=hyprland --description="hyprland start service" hyprland ; fi' >> /etc/profile.d/launch_hyprland.sh
