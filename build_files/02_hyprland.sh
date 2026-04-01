706a7a62a19a848d0621ee16f39af8451bcf83d0
ManInDark <61268856+ManInDark@users.noreply.github.com>
2026-04-01 21:41:42 +0200

    feat: start hyprland as systemd user service (#8)

--- build_files/02_hyprland.sh
+++ build_files/02_hyprland.sh
@@ -16,4 +16,5 @@ dnf5 install -y \
     SwayNotificationCenter-git \
     foot
 
-echo 'if [[ $(ps aux | grep hyprland | wc -l) -eq 1 ]]; then cd $HOME && echo "launch hyprland?" && read && systemd-run --user --service-type=exec --unit=hyprland --description="hyprland start service" hyprland ; fi' >> /etc/profile.d/launch_hyprland.sh
+cp /ctx/hyprland.service /usr/lib/systemd/user/hyprland.service
+systemctl --global enable hyprland.service
